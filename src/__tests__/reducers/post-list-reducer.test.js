f9bd87fd981044797c129f6e7f2783a6566be74e
Daniel Merys <dkmerys@gmail.com>
2020-07-07 13:58:13 -0700

    successfully pass tests for add and delete post functionality using redux

--- src/__tests__/reducers/post-list-reducer.test.js
+++ src/__tests__/reducers/post-list-reducer.test.js
@@ -1,7 +1,57 @@
 import postListReducer from '../../reducers/post-list-reducer';
 
+
+
 describe('postListReducer', () => {
+
+  let action;
+  const currentState = {
+    1:{
+      userName: 'Shaquille Oatmeal',
+      message: 'Oh man, it is only Wednesday?',
+      id: 1 },
+    2:{
+      userName: 'Charvarious Ward',
+      message: 'Yep. Wednesday.',
+      id: 2 }
+    }
+
+    const postData = {
+      userName: 'Shaquille Oatmeal',
+      message: 'We all make mistakes.',
+      id: 1
+    };
+
   test('Should return default state if there is no action type passed into the reducer', () => {
     expect(postListReducer({}, { type: null })).toEqual({});
+  });
+  test('Should successfully add a new post to PostList', () => { 
+    const { userName, message, id } = postData;
+    action = {
+      type: 'ADD_POST',
+      userName: userName,
+      message: message,
+      id: id
+    };
+    expect(postListReducer({}, action)).toEqual({
+      [id] : {
+        userName: userName,
+        message: message,
+        id: id,
+      }
+    });
+  });
+  test('Should successfully delete a post', () => {
+    action = {
+      type: 'DELETE_POST',
+      id: 1 
+    };
+    expect(postListReducer(currentState, action)).toEqual({
+      2: {
+        userName: 'Charvarious Ward',
+        message: 'Yep. Wednesday.',
+        id: 2 
+      }
+    });
   });
 });
--- src/reducers/post-list-reducer.js
+++ src/reducers/post-list-reducer.js
@@ -1,3 +1,19 @@
 export default (state = {}, action) => {
-
+  const { userName, message, id } = action;
+  switch (action.type) { 
+    case 'ADD_POST':
+      return Object.assign({}, state, {
+        [id]: {
+          userName: userName,
+          message: message,
+          id: id,
+        }
+      });
+    case 'DELETE_POST':
+      const newState = { ...state };
+      delete newState[id];
+      return newState;
+    default:
+      return state
+  }
 };
