485fd37a3838c612a11a46a17b91b167f49012a9
Daniel Merys <dkmerys@gmail.com>
2020-07-07 15:56:58 -0700

    successfully pass 2 smoke tests in root reducer

--- src/__tests__/reducers/index-reducer.test.js
+++ src/__tests__/reducers/index-reducer.test.js
@@ -1,4 +1,9 @@
 import rootReducer from '../../reducers/index';
+import { createStore } from 'redux';
+import postUpvoteReducer from '../../reducers/post-upvote-reducer';
+import postListReducer from '../../reducers/post-list-reducer';
+
+let store = createStore(rootReducer);
 
 describe('rootReducer', () => {
   test('Should return default state if no action type is recognized', () => {
@@ -7,4 +12,14 @@ describe('rootReducer', () => {
       handleUpvotePost: {}
     });
   });
+  
+  test('Check that initial state of postListReducer matches root reducer', () => {
+    expect(store.getState().postList).toEqual(postListReducer(undefined, { type: null }));
+  });
+
+  test('Check that initial state of postUpvoteReducer matches root reducer', () => {
+    expect(store.getState().handleUpvotePost).toEqual(postUpvoteReducer(undefined, { type: null }));
+  });
+
+  test()
 });
